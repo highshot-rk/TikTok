e804c12399628fc4593daa2756ca0d00f5347c9d
highshot-rk <sagarn62018@gmail.com>
2022-10-29 00:00:11 -0300

    init

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+// COLORS
+
+const backgroundColor = Colors.white;
+const buttonColor = Colors.redAccent;
+const borderColor = Colors.grey;
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok/constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'TikTok',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      home: const Text('Hello World!'),
+    );
+  }
+}
